ab507f71f01255f15d42cb8850739117ab2aef13
cibt0943 <cibt0943@users.noreply.github.com>
2024-10-28 22:33:50 +0900

    ライブラリアップデート

--- chimera-app/eslint.config.js
+++ chimera-app/eslint.config.js
@@ -59,8 +59,8 @@ export default tseslint.config(
     rules: {
       ...pluginReact.configs.recommended.rules,
       ...pluginReact.configs['jsx-runtime'].rules,
-      ...pluginJsxA11y.configs.recommended.rules,
       ...pluginReactHooks.configs.recommended.rules,
+      ...pluginJsxA11y.configs.recommended.rules,
       'react/prop-types': 'off', // Props の型チェックは TS で行う & 誤検知があるため無効化
     },
     settings: {
