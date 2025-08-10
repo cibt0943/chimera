d758efecaebfd73c1f92ffb4e1e17a9478bbbdf2
cibt0943 <cibt0943@users.noreply.github.com>
2025-08-10 22:53:00 +0900

    experssのメジャーバージョンアップ:4→5

--- chimera-app/vite.config.ts
+++ chimera-app/vite.config.ts
@@ -1,8 +1,15 @@
 import { reactRouter } from '@react-router/dev/vite'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: './server/app.ts',
+        }
+      : undefined,
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-})
+}))
